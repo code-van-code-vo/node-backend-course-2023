cb13c9640712cc6ccc9540a2e8654d68a60221bb
balldk <pr0f3ss0r.b@gmail.com>
2023-07-11 20:27:28 +0700

    update day 1

--- day 1/2 - lottery app.js
+++ day 1/2 - lottery app.js
@@ -4,3 +4,27 @@
 //   users: array of users
 // Output:
 //   pick a random winning user in users
+
+const users = [
+    { name: 'khoa', age: 20 }
+]
+
+function insertUser(n, a) {
+    const user = {
+        name: n,
+        age: a,
+    }
+    users.push(user)
+}
+
+function randElementInArray(arr) {
+    const randomIndex = Math.floor(Math.random()*arr.length)
+    return arr[randomIndex]
+}
+
+insertUser('Binh', 21)
+insertUser('Giang', 19)
+insertUser('Tan', 18)
+
+const winningUser = randElementInArray(users)
+console.log(`${winningUser.name} has won a lottery ticket at age ${winningUser.age}`)
