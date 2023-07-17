1e580ba7beaa87d454a88653918a947b3b9424c9
balldk <pr0f3ss0r.b@gmail.com>
2023-07-18 00:58:36 +0700

    day 1 homework solution

--- day 1/5 - fetch data.js
+++ day 1/5 - fetch data.js
@@ -1,7 +1,5 @@
-
-
 const axios = require('axios')
-const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')()
 
 function fetchQuestions(nQuestion, callback) {
     axios.get(`https://opentdb.com/api.php?amount=${nQuestion}`).then((res) => {
@@ -9,10 +7,62 @@ function fetchQuestions(nQuestion, callback) {
     })
 }
 
-fetchQuestions(2, (questions) => {
-    // 2 lines below is how we can get user input from keyboard
-    // let userInput = prompt('question: ')
-    // console.log(userInput)
+// Copy from Stackoverflow
+function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex
 
-    console.log(questions)
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+
+        // And swap it with the current element.
+        ;[array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ]
+    }
+
+    return array
+}
+
+fetchQuestions(10, (questions) => {
+    let nCorrect = 0
+    questions.forEach((data) => {
+        // spread operator
+        let answers = [...data.incorrect_answers, data.correct_answer]
+        // let answers = [
+        //     data.incorrect_answers[0],
+        //     data.incorrect_answers[1],
+        //     data.incorrect_answers[2],
+        //     data.incorrect_answers[3],
+        //     data.correct_answer
+        // ]
+        answers = shuffle(answers)
+
+        console.log(data.question)
+        console.log(`A. ${answers[0]}`)
+        console.log(`B. ${answers[1]}`)
+
+        if (data.type === 'multiple') {
+            console.log(`C. ${answers[2]}`)
+            console.log(`D. ${answers[3]}`)
+        }
+
+        const userAnswer = prompt('Your answer is: ')
+        const userAnswerIndex = userAnswer.charCodeAt(0) - 65
+
+        if (answers[userAnswerIndex] === data.correct_answer) {
+            nCorrect += 1
+            console.log('Your answer is correct!')
+        } else {
+            console.log(
+                `Sorry but the correct answer is ${data.correct_answer}`
+            )
+        }
+        console.log()
+    })
+    console.log(`You have ${nCorrect}/${questions.length} correct answers`)
 })
